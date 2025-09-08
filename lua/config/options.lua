fd19ef96f793b8ae58c7eece75c42959e84880ce
henzoparahua <hnz.2894henzop19@gmail.com>
2025-09-08 00:03:37 -0300

    commit

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,5 +12,9 @@ vim.g.transparent_enabled = true
 vim.opt.laststatus = 2 -- Always show statusline
 vim.opt.termguicolors = true -- Enable true colors (required for staline)
 
+-- Line number configuration
+vim.opt.number = true         -- Show absolute line numbers
+vim.opt.relativenumber = false -- Disable relative line numbers
+
 -- Load transparency configuration
 require("config.transparency")
--- lua/plugins/staline.lua
+++ lua/plugins/staline.lua
@@ -8,9 +8,9 @@ return {
 
     require("staline").setup({
       sections = {
-        left = { '  ', 'mode', ' ', 'branch', ' ', 'lsp' },
-        mid = {},
-        right = {'file_name', 'line_column' }
+        left = { " ", "mode", " ", "branch" },
+        mid = { "lsp" },
+        right = { "file_name", "line_column" },
       },
       mode_colors = {
         i = "#d4be98",
@@ -21,8 +21,8 @@ return {
       defaults = {
         true_colors = true,
         line_column = " [%l/%L] :%c  ",
-        branch_symbol = " "
-      }
+        branch_symbol = " ",
+      },
     })
     vim.cmd([[ hi Evil guifg=#f36365 guibg=NONE ]])
     vim.cmd([[ hi StalineEnc guifg=#7d9955 guibg=NONE ]])
